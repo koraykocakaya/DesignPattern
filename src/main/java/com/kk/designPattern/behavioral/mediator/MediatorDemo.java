b724fd4ac85b247b40a91017a941725f0faa9fdf
koray.kocakaya <koray.kocakaya@cybersoft.com.tr>
2022-01-27 07:55:59 +0300

    Null Object Design Pattern added

--- src/main/java/com/kk/designPattern/behavioral/mediator/MediatorDemo.java
+++ src/main/java/com/kk/designPattern/behavioral/mediator/MediatorDemo.java
@@ -5,44 +5,39 @@ import java.util.List;
 
 public class MediatorDemo {
 	public static void main(String[] args) {
-		
+
 		Mediator mediator = new Mediator();
 		Participant p1 = new Participant(mediator);
 		Participant p2 = new Participant(mediator);
-		
+
 		p1.say(10);
 		System.out.println(p1.value);
 		System.out.println(p2.value);
-		
+
 		p2.say(4);
 		System.out.println(p1.value);
 		System.out.println(p2.value);
 	}
 }
 
-class Participant
-{
-  int value = 0;
-  Mediator mediator;
-  public Participant(Mediator mediator)
-  {
-	  this.mediator = mediator;
-    mediator.participantList.add(this);
-  }
+class Participant {
+	int value = 0;
+	Mediator mediator;
 
-  public void say(int n)
-  {
-	  mediator.broadcast(this, n); 
-  }
+	public Participant(Mediator mediator) {
+		this.mediator = mediator;
+		mediator.participantList.add(this);
+	}
+
+	public void say(int n) {
+		mediator.broadcast(this, n);
+	}
 }
 
-class Mediator
-{
-  List<Participant> participantList = new ArrayList<>();
-  
-  public void broadcast(Participant participant, int increasedValue){
-	  participantList.stream()
-	  				 .filter(p -> !p.equals(participant))
-	  				 .forEach(p -> p.value += increasedValue);
-  }
+class Mediator {
+	List<Participant> participantList = new ArrayList<>();
+
+	public void broadcast(Participant participant, int increasedValue) {
+		participantList.stream().filter(p -> !p.equals(participant)).forEach(p -> p.value += increasedValue);
+	}
 }
